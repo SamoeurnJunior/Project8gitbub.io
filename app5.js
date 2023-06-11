c8075e8fb0acd0c5cda937aedcc89d1d3b36ee9d
SamoeurnJunior <135082824+SamoeurnJunior@users.noreply.github.com>
2023-06-11 08:18:29 -0700

    Add files via upload
    Can someone test this

---
+++ app5.js
@@ -0,0 +1,23 @@
+const formOpenBtn = document.querySelector("#form-open"),
+home= document.querySelector(".home"),
+formContainer= document.querySelector(".form_container"),
+formCloseBtn= document.querySelector(".form_close"),
+signupBtn= document.querySelector("#signup"),
+loginBtn= document.querySelector("#login"),
+pwShowHide= document.querySelectorAll(".pw_hide");
+
+formOpenBtn.addEventListener("click", () => home.classList.add("Show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("Show"));
+
+
+
+
+
+signupBtn.addEventListener("click", (e) => {   
+    e.preventDefault();
+    formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {   
+    e.preventDefault();
+    formContainer.classList.remove("active");
+});
